0ed9b8b4811c769cec742389a38a0b4055b438a5
Stephen Prescott <scobes@gmail.com>
2020-08-14 14:13:28 +0200

    verify sync works as expected

---
+++ tos/index.php
@@ -0,0 +1,38 @@
+<?php
+
+// include the includes
+require_once('../tosres/includes.inc');
+
+
+// check the session, if none go do setup
+
+
+// if there's input send it to the parser
+
+
+// check player is alive
+
+
+// get current room
+
+
+// describe room and situation
+
+
+// select input options
+
+
+// write world to session
+
+
+// display interface
+
+function stuffOnScreen() {
+	?>
+<pre> Nothing here yet </pre>
+
+<?php 
+}
+	
+stuffOnScreen();	
+?>
