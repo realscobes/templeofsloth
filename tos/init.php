0983f856177dfc01cc9dc6b013bb3c740406bb7b
Stephen Prescott <scobes@gmail.com>
2020-08-15 04:45:09 +0200

    debug and discarded code removed

--- tos/init.php
+++ tos/init.php
@@ -5,7 +5,7 @@ extract(parse_ini_file (TOSRES.'settings/initsettings.ini'));
 $thingDensity = array ('monsters' => $monstPercent, 
                        'chests' => $chestPercent, 
                        'pools' => $poolPercent, 
-                       'warp' => $warpPercent);
+                       'warps' => $warpPercent);
 
 $world = new World ($mapX, $mapY, $mapFlr, $thingDensity);
 
